cc9628a7caf06aeac2c3e305ba6fcabd716ce495
Israel Cruz <israel.crz@outlook.com>
2021-04-01 04:49:13 -0600

    Create task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -6,6 +6,12 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
     context = {
         'tasks':tasks,
