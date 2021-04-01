17c8b9a4d04fb7c09ff1a2346f128fc094ad85aa
Israel Cruz <israel.crz@outlook.com>
2021-04-01 04:44:27 -0600

    Model Form created

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.all()
 
+    form = TaskForm()
+
     context = {
         'tasks':tasks,
+        'form':form,
     }
     
     return render(request, "tasks/tasks.html", context)
